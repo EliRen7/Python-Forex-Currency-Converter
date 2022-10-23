2c899a154261851002ce8d9532a32bf366817d6c
Elizabeth Renderos <84453538+EliRen7@users.noreply.github.com>
2022-10-23 15:09:34 +0900

    Updated app.py

--- app.py
+++ app.py
@@ -20,27 +20,10 @@ def get_currency():
     final = round(rates.convert(converting_from, converting_to, value),2)
     return render_template('rate.html', final_rate = final, symbol=symbol)
 
-# @app.route('/', methods=['POST'])
-# def process():
-#     conv_from = request.form['convert_from']
-#     conv_to = request.form['convert_to']
-#     if request.form['amount'] == "":
-#         print("Enter a valid amount")
-#         return False
-#     else:
-#         amount = float(request.form['amount'])
-    
-#     try:
-#         results = round(rates.convert(conv_from, conv_to, amount), 2)
-#         symbol = codes.get_symbol(conv_to)
-#         return render_template("rate.html", convert_from=conv_from, convert_to=conv_to, amount=amount, final_rate=results, symbol = symbol)
-#     except RatesNotAvailableError:
-#         flash("Please enter a valid currency")
-#         return redirect('/')
-#     except (ValueError, TypeError):
-#         flash("Oops something went wrong")
-#         return redirect('/')
-   
+
+
+
+
 
 
 
